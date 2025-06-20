84fece7cf37a7b2ba985f5ff949ed9cc09445a58
HariharanThirupp <124854842+HariharanThirupp@users.noreply.github.com>
2025-06-20 19:02:10 +0530

    Add files via upload

---
+++ Week 1/1.Design Patterns and Principles (Exercise 1 Implementing the Singleton Pattern)/EagerSingleton/EagerSingleton.java
@@ -0,0 +1,12 @@
+package default;
+class EagerSingleton {
+    private static final EagerSingleton instance = new EagerSingleton();
+
+    private EagerSingleton() {
+        System.out.println("EagerSingleton instance created");
+    }
+
+    public static EagerSingleton getInstance() {
+        return instance;
+    }
+}
