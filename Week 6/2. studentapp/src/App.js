cadcfde0fe85d8934863b10b78d9ce1cc4bf1d3b
HariharanThirupp <124854842+HariharanThirupp@users.noreply.github.com>
2025-07-27 20:57:22 +0530

    Add files via upload

---
+++ Week 6/2. studentapp/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Componet/Home';
+import About from './Componet/About';
+import Contact from './Componet/Contact';
+function App() {
+  return (
+    <div className="container">
+      <Home/>
+      <About/>
+      <Contact/>
+      </div>
+  );
+}
+
+export default App;
